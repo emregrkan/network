bea701389fab54ae84c08737466567beb72b2409
Emre Gürkan <emre-gurkan@hotmail.com>
2023-12-19 00:04:19 +0300

    Added UDP server

--- gateway/internal/server/server.go
+++ gateway/internal/server/server.go
@@ -15,11 +15,22 @@ type Server interface {
 	Run()
 }
 
-type UDPServer struct{}
+type UDPServer struct {
+	Port string
+	// To process and send response to the client
+	sensAddr   net.Addr
+	packetConn net.PacketConn
+}
+
+type udpReq struct {
+	net.Addr
+	Len  int
+	Body []byte
+}
 
 type TCPServer struct {
 	Port     string
-	tempConn net.Conn
+	sensConn net.Conn
 	buffer   []string
 	// server connection
 }
@@ -60,7 +71,7 @@ func (tr *tcpReader) readConnection(reqChan chan []byte, errChan chan error) {
 
 func (srv *TCPServer) sendResponse(status int, req string) {
 	// Use with valid connection
-	if _, err := fmt.Fprint(srv.tempConn, status); err != nil {
+	if _, err := fmt.Fprint(srv.sensConn, status); err != nil {
 		log.Println("Error sending response")
 	}
 	log.Println("Response status", status, "sent for request:", req)
@@ -98,25 +109,30 @@ func (srv *TCPServer) Run() {
 		log.Fatal("Failed to create TCP listener:", err)
 	}
 
-	// Close the listener
-	defer ln.Close()
+	// Close the listener and connection if not closed
+	defer func() {
+		ln.Close()
+		if srv.sensConn != nil {
+			srv.sensConn.Close()
+		}
+	}()
 
 	// Accept connection attempts
 	// Note: TCP Server accepts a single active connection
-	for srv.tempConn == nil {
-		srv.tempConn, err = ln.Accept()
+	for srv.sensConn == nil {
+		srv.sensConn, err = ln.Accept()
 		if err != nil {
 			log.Println("Failed to connect to the client:", err)
 		}
 
-		tr := &tcpReader{Conn: srv.tempConn}
+		tr := &tcpReader{Conn: srv.sensConn}
 
 		reqChan := make(chan []byte)
 		errChan := make(chan error)
 
 		go tr.readConnection(reqChan, errChan)
 
-		for srv.tempConn != nil {
+		for srv.sensConn != nil {
 			select {
 			// Received request successfully
 			case req := <-reqChan:
@@ -127,11 +143,11 @@ func (srv *TCPServer) Run() {
 				if errors.Is(err, io.EOF) || errors.Is(err, syscall.ECONNRESET) {
 					log.Println("Connection reset")
 					// Closing the connection
-					if err = srv.tempConn.Close(); err != nil {
+					if err = srv.sensConn.Close(); err != nil {
 						log.Fatal(err)
 					}
 					// Set the connection to nil so can server accept a new connection
-					srv.tempConn = nil
+					srv.sensConn = nil
 				} else {
 					// Possible unrecoverable error
 					// TODO: might send status 500
@@ -139,6 +155,82 @@ func (srv *TCPServer) Run() {
 					// Shuts down the business
 					return
 				}
+			}
+		}
+	}
+}
+
+func (srv *UDPServer) listenRequests(reqChan chan *udpReq, errChan chan error) {
+	// Keep accepting packets as long as there is a client (sensor)
+	for srv.packetConn != nil {
+		// 128 byte long packets should be enough
+		buff := make([]byte, 128)
+		n, addr, err := srv.packetConn.ReadFrom(buff)
+		if n == 0 || err != nil {
+			errChan <- err
+			// To leave the desicion to the caller
+			continue
+		}
+		// Len is to get rid of NULLs
+		// I'm not exactly sure about n-1 is the best way to get rid of LF
+		reqChan <- &udpReq{Addr: addr, Len: n - 1, Body: buff}
+	}
+}
+
+func (srv *UDPServer) Run() {
+	var err error
+	// Listen UDP packets
+	srv.packetConn, err = net.ListenPacket("udp", srv.Port)
+	if err != nil {
+		log.Fatal("Failed to create UDP listener")
+	}
+
+	// Close UDP server
+	defer func() {
+		srv.packetConn.Close()
+		// To terminate `listenRequests` goroutine
+		srv.packetConn = nil
+	}()
+
+	// Client is nil when there is no sensor
+	srv.sensAddr = nil
+
+	reqChan := make(chan *udpReq)
+	errChan := make(chan error)
+
+	go srv.listenRequests(reqChan, errChan)
+
+	// I'm not very happy about this
+	timer := time.NewTimer(0)
+	defer timer.Stop()
+
+	for {
+		select {
+		// Received a request
+		case req := <-reqChan:
+			// Initial request set up
+			if srv.sensAddr == nil {
+				srv.sensAddr = req.Addr
+				// Timer starts here
+				timer.Reset(7 * time.Second)
+			}
+			reqStr := string(req.Body[:req.Len])
+			if reqStr == "ALIVE" {
+				// `ALIVE` message received reset the timer
+				timer.Reset(7 * time.Second)
+			}
+			log.Println(reqStr)
+		// An error occured during processing request
+		case err := <-errChan:
+			log.Println(err)
+			return
+		// Possible (almost certain) time out
+		case <-timer.C:
+			// I don't know if this check is necessary
+			if srv.sensAddr != nil {
+				log.Println("Humidty sensor down")
+				// Reset for the next inital set up
+				srv.sensAddr = nil
 			}
 		}
 	}
--- gateway/main.go
+++ gateway/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "emregrkan.network.io/gateway/internal/server"
+import (
+	"emregrkan.network.io/gateway/internal/server"
+)
 
 func main() {
-	srv := &server.TCPServer{
+	tcpSrv := &server.TCPServer{
 		Port: ":8367",
 	}
-	srv.Run()
+	udpSrv := &server.UDPServer{
+		Port: ":4638",
+	}
+	go udpSrv.Run()
+	tcpSrv.Run()
 }
