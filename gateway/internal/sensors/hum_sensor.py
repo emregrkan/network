18aa82de2c11fcba185129b367525272f4ffcfb5
Batuhan Baştürk <batuhanbasturk636@gmail.com>
2023-12-19 19:05:54 +0300

    humidity sensor created

---
+++ gateway/internal/sensors/hum_sensor.py
@@ -0,0 +1,27 @@
+import socket
+import time
+import random
+
+udp_host = "localhost" 
+udp_port = 4638 
+
+def humidity_sensor(): 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    while True:
+        humidity_value = random.randint(40, 90)
+        # Send information if humidity exceeds 80
+        if humidity_value > 80:
+            message = f"{humidity_value}"
+            sock.sendto(message.encode(), (udp_host, udp_port))
+            print(f"Sent --> Humidity: {message}")
+
+        # Send 'ALIVE' message every 3 seconds
+        time.sleep(1)
+        if time.time() % 3 == 0:
+            alive_message = "ALIVE"
+            sock.sendto(alive_message.encode(), (udp_host, udp_port))
+            print(f"Sent: {alive_message}")
+
+if __name__ == "__main__":
+    humidity_sensor()
